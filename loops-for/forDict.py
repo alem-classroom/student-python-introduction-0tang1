6b740ae2c51297fc5e4115ee36b954477a155a51
Tangzharyk <90594377+0tang1@users.noreply.github.com>
2021-10-30 20:33:18 +0000

    Completed arrayLoop and forDict files.

--- loops-for/forDict.py
+++ loops-for/forDict.py
@@ -1,2 +1,9 @@
 def reverse_dict(dict):
-    # swap keys and values within dict and return dict
+  # swap keys and values within dict and return dict
+  for k, v in dict.items():
+    v:k
+  return dict
+
+
+# dict = {1:'a', 2:'b', 3:'c'}
+# print(reverse_dict(dict))
