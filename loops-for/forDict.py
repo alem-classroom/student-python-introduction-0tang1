609b6511b31e34412a420e0d93234e922c75bb04
Tangzharyk <90594377+0tang1@users.noreply.github.com>
2021-10-30 20:49:49 +0000

    updated solution

--- loops-for/forDict.py
+++ loops-for/forDict.py
@@ -1,8 +1,13 @@
 def reverse_dict(dict):
   # swap keys and values within dict and return dict
+  old_dict = dict
+  new_dict = {}
   for k, v in dict.items():
-    v:k
-  return dict
+    if v in new_dict:
+      new_dict[v].append(k)
+    else:
+      new_dict[v]=k
+  return new_dict
 
 
 # dict = {1:'a', 2:'b', 3:'c'}
